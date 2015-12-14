e2626494e4bcc51535538e9fdc3818fa15afce6c
Dawei Xia <xiadw_public@163.com>
2015-12-14 18:50:28 +0800

    update gulpfile.js

--- frontend-nanodegree-mobile-portfolio-master/gulpfile.js
+++ frontend-nanodegree-mobile-portfolio-master/gulpfile.js
@@ -1,8 +1,24 @@
 var gulp = require('gulp');
-var critical = require('critical').stream;
 
+var minifyHTML = require("gulp-minify-html");
+//var critical = require('critical').stream;
+
+/*
 gulp.task('default', function(){
 	return gulp.src('original-index.html')
 	.pipe(critical({base: 'css/', inline: true, minify: true, css:['css/style.css'], width: 1300, height: 900}))
 	.pipe(gulp.dest('dist'));
+});
+
+*/
+
+gulp.task("default", function(){
+	var opts = {
+		conditionals: true,
+		spare: true
+	};
+
+	return gulp.src('index.html')
+	.pipe(minifyHTML(opts))
+	.pipe(gulp.dest('dest/index.html'));
 });
