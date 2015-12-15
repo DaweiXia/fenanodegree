6d743165172f316b7ee6d8cea677a6f570cf2a59
Dawei Xia <xiadw_public@163.com>
2015-12-15 22:07:42 +0800

    Solve FSL by getting scroolTopRatio out of the for loop

--- frontend-nanodegree-mobile-portfolio-master/views/js/main.js
+++ frontend-nanodegree-mobile-portfolio-master/views/js/main.js
@@ -503,8 +503,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.getElementsByClassName('mover');
+  var scrollTopRatio = document.body.scrollTop / 1250;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin( scrollTopRatio + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
