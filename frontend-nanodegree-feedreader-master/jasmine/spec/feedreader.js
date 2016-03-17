1445a4d3d64807d8bf63f0f524338d7df725d104
Dawei Xia <xiadw_public@163.com>
2016-03-17 21:16:34 +0800

    Write a test that ensures when the loadFeed function is called and completes its work, there is at least a single .entry element within the .feed container.

--- frontend-nanodegree-feedreader-master/jasmine/spec/feedreader.js
+++ frontend-nanodegree-feedreader-master/jasmine/spec/feedreader.js
@@ -86,7 +86,15 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done){
+            loadFeed(0, done);
+         });
+
+         it('should have at least one entry within the feed container after loadFeed function is called', function() {
+            expect($('.feed').length).toBeGreaterThan(0);
+         });
     })
+    
     /* TODO: Write a new test suite named "New Feed Selection"
 
         /* TODO: Write a test that ensures when a new feed is loaded
